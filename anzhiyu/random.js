15ca7f56a7fad4b7b922cffac06f2051543f02c8
HeyBear-5919 <heybear5919@gmail.com>
2025-05-24 19:32:23 +0800

    Site updated: 2025-05-24 19:32:21

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/05/09/hello-world/"];function toRandomPost(){
+var posts=["undefinedhello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
